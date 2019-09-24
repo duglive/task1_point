df3a5ae8c2d0869682e2413512b90475efc771ff
Said <ssdagalaev@edu.hse.ru>
2019-09-24 22:58:24 +0300

    Vse sdelal. Said

---
+++ src/cmake-build-debug/CMakeFiles/xipoint_main.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/xipoint_main.dir/main.cpp.o"
+  "CMakeFiles/xipoint_main.dir/xipoint.cpp.o"
+  "xipoint_main.pdb"
+  "xipoint_main"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/xipoint_main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/xipoint.cpp
+++ src/xipoint.cpp
@@ -17,10 +17,178 @@
 
 namespace xi {
 
+    void copy(const Point* from, Point* to, PointArray::Uint size)
+    {
+        for (int i = 0; i < size; ++i)
+        {
+            to[i] = from[i];
+        }
+    }
 
+    Point::Point(int x, int y)
+    {
+        _x = x;
+        _y = y;
+    }
 
-// TODO: put the method implementations here
+    void Point::shift(int shf)
+    {
+        _x += shf;
+        _y += shf;
+    }
 
+    void Point::shift(int xSh, int ySh)
+    {
+        _x += xSh;
+        _y += ySh;
+    }
 
+    void Point::shift(const Point& pt)
+    {
+        _x += pt.getX();
+        _y += pt.getY();
+    }
+
+    double Point::dist(const xi::Point& other) const
+    {
+        return sqrt(pow(_x - other._x, 2) + pow(_y - other._y, 2));
+    }
+
+    int Point::getX() const
+    {
+        return _x;
+    }
+
+    int Point::getY() const
+    {
+        return _y;
+    }
+
+    void Point::setX(int x)
+    {
+        _x = x;
+    }
+
+    void Point::setY(int y)
+    {
+        _y = y;
+    }
+
+    PointArray::PointArray()
+    {
+        _size = 0;
+        _arr = nullptr;
+    }
+
+    PointArray::PointArray(const Point points[], PointArray::Uint n)
+    {
+        _size = n;
+        _arr = new Point[_size];
+
+        copy(points, _arr, _size);
+    }
+
+    PointArray::PointArray(const PointArray& pv)
+    {
+        _size = pv.getSize();
+        _arr = new Point[_size];
+        const Point *temp = pv.getRawPtr();
+
+        copy(temp, _arr, _size);
+    }
+
+    PointArray::~PointArray()
+    {
+        clear();
+    }
+
+    void PointArray::append(const Point& pt)
+    {
+        resize(_size + 1);
+        _arr[_size - 1] = pt;
+    }
+
+    void PointArray::insert(const Point& pt, PointArray::Uint pos)
+    {
+        if ((pos >= 0) && (pos < _size))
+        {
+            resize(_size + 1);
+
+            for (int i = _size - 1; i > pos; --i)
+            {
+                _arr[i] = _arr[i - 1];
+            }
+
+            _arr[pos] = pt;
+        }
+
+        else if (pos >= _size)
+        {
+            append(pt);
+        }
+    }
+
+    void PointArray::remove(PointArray::Uint pos)
+    {
+        if ((pos >= 0) && (pos < _size))
+        {
+            for (int i = pos; i < _size - 1; ++i)
+            {
+                _arr[i] = _arr[i + 1];
+            }
+
+            resize(_size - 1);
+        }
+    }
+
+    void PointArray::clear()
+    {
+        if (_arr != nullptr)
+        {
+            delete[] _arr;
+            _arr = nullptr;
+        }
+        _size = 0;
+    }
+
+    PointArray::Uint PointArray::getSize() const
+    {
+        return _size;
+    }
+
+    Point* PointArray::get(PointArray::Uint pos)
+    {
+        if ((pos < 0) || (pos >= _size))
+        {
+            return nullptr;
+        }
+
+        return _arr + pos;
+    }
+
+    const Point* PointArray::get(PointArray::Uint pos) const
+    {
+        if ((pos < 0) || (pos >= _size))
+        {
+            return nullptr;
+        }
+
+        return _arr + pos;
+    }
+
+    void PointArray::resize(PointArray::Uint n)
+    {
+        Point* tempArr = new Point[n];
+        Uint sizeToCopy = std::min(_size, n);
+
+        for (int i = 0; i < sizeToCopy; ++i)
+        {
+            tempArr[i] = _arr[i];
+        }
+
+        _size = n;
+        delete[] _arr;
+        _arr = tempArr;
+    }
 
 } // namespace xi {
